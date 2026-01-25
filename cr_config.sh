f62ddbcf3b962b263698c715b09fc828358dc61c
Moises Soto <35436843+moisoto@users.noreply.github.com>
2026-01-25 18:07:51 -0400

    Add cr_config.sh script.

---
+++ cr_config.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+# Set variables
+configfile=$(chezmoi data --format=yaml | grep "configFile:" | awk '{ print $2 }')
+fileext="${configfile##*.}"
+filedir="${configfile%/*}"
+gpgkey=$(gpg --list-secret-keys --keyid-format LONG --with-colons | awk -F: '/^sec/ {print $5}' | tail -1)
+fullkey=$(gpg --list-secret-keys --keyid-format LONG --with-colons | grep "$gpgkey" | awk -F: '/^fpr/ {print $10}')
+
+if [ "$fileext" != "toml" ]; then
+  echo "Extension $fileext not supported."
+  exit 1
+fi
+
+if [[ -f "$configfile" && "$1" != "--force" ]]; then
+  echo "Config file $configfile already exist."
+  echo "Please edit manually with: \"chezmoi edit-config\"."
+  echo
+  exit 1
+fi
+
+if [ ! -d "$filedir" ]; then
+  echo "Creating config folder $filedir."
+  mkdir -p $filedir
+fi
+
+echo "Creating config file at: $configfile"
+read -r -p "Proceed? [Y/n] " answer
+
+case "$answer" in
+  [Yy]|"")
+    echo "[data.config.machine]" > $configfile
+    echo "name  = \"$(scutil --get LocalHostName)\"" >> $configfile
+    echo "owner = \"$USER\"" >> $configfile
+    echo "role  = \"laptop|desktop|office\"" >> $configfile
+    echo "id    = \"${USER}.$(scutil --get LocalHostName)\"" >> $configfile
+    echo >> $configfile
+    echo "[data.config.secrets]" >> $configfile
+    if [ -z "$gpgkey" ] ; then
+      echo "gpg_key    = \"put-your-gpp-here\"" >> $configfile
+      echo "gpg_defkey = \"put-your-default-gpg-here\"" >> $configfile
+    else
+      echo "gpg_key    = \"$gpgkey\"" >> $configfile
+      echo "gpg_defkey = \"$fullkey\"" >> $configfile
+    fi
+    echo "gemini_key = \"put-your-gemini-apikey-here\"" >> $configfile
+    echo "Config file created."
+    ;;
+  *)
+    exit 1
+    ;;
+esac
+echo
