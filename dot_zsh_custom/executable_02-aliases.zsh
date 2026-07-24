6734f041a908702cd73cf7fa071690f5c89378fc
Moises Soto <35436843+moisoto@users.noreply.github.com>
2026-07-24 10:27:54 -0400

    Add gcso (git config --show-origin) alias

--- dot_zsh_custom/executable_02-aliases.zsh
+++ dot_zsh_custom/executable_02-aliases.zsh
@@ -8,6 +8,7 @@ alias lst="ls -lrt"
 alias lzt="eza -l --header --icons --git --group-directories-first --sort=modified --color=always --tree --level=3"
 alias lz="eza  -l --header --icons --git --group-directories-first --sort=modified --color=always"
 alias grso="git remote show origin"
+alias gcso="git config --list --show-origin --show-scope"
 alias gdf="~/git-utils/git_df.sh"        # Also available as git alias (use "git df" instead)
 alias gcommit="~/git-utils/commit.sh"    # Also available as git alias (use "git gcm" instead)
 alias trail-trim="~/git-utils/git_tt.sh" # Also available as git alias (use "git tt" instead)
